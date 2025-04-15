bee0ac97099f279a4e108d171796332dfd375fbd
cj-spence <christopherspencejr@gmail.com>
2025-04-15 08:57:56 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Landing } from './pages/Landing'
+import './App.css'
+import './index.css'
+import Navbar from './components/Navbar'
+import { Example } from './pages/Example'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Landing />
+      <Example />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+      <div className="navbar fixed z-50 shadow-sm bg-base-100/50 backdrop-blur-lg">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> 
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> 
+            </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Home</a>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Navbar;
