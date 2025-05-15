198a5ab4f4824a57fd3b5c2d2bc533de889054a0
cj-spence <christopherspencejr@gmail.com>
2025-05-15 11:00:05 -0700

    fixed scrolldown

--- src/components/ScrollDown.jsx
+++ src/components/ScrollDown.jsx
@@ -4,16 +4,15 @@ import { FaChevronDown } from "react-icons/fa";
 
 export const ScrollDown = () => {
   const ref = useRef(null);
-
   const { scrollY } = useScroll();
 
-  const opacity = useTransform(scrollY, [0, 700], [1, 0]);
+  const opacity = useTransform(scrollY, [0, 500], [1, 0]);
 
   return (
     <motion.div
       ref={ref}
       style={{ opacity }}
-      className="absolute bottom-4 left-1/2 transform -translate-x-1/2"
+      className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50"
     >
       <div className="animate-bounce text-4xl text-black md:text-4xl lg:text-5xl">
         <FaChevronDown />
